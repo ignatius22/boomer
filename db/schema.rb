d5663b770673f1b096ad8251b6b69fd3224fbfa8
ignatius22 <igee54077@gmail.com>
2023-08-09 18:41:13 +0100

    Run migration for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_152754) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_173854) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
