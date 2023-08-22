352ec79d7967ac39589df09a10cddc86d0bf11eb
ignatius22 <igee54077@gmail.com>
2023-08-23 00:48:41 +0100

    Add stylings to the user update page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :bio, :profile_photo])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :bio])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :bio, :profile_photo])
     end
 end
