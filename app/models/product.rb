e7be443df11ef23bb448e670a04dfbadaf20bb90
ignatius22 <igee54077@gmail.com>
2023-08-23 09:53:30 +0100

    Add products model and controller

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_one_attached :image
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+end
