15adb6a5680deade66b314c435d0c27c13f8e2cc
ignatius22 <igee54077@gmail.com>
2023-09-08 16:47:37 +0100

    Fix the add method in the cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -13,16 +13,11 @@ class CartController < ApplicationController
       current_order.destroy
     else
       @cart.orders.create(product: @product, quantity:)
+      
     end
 
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [turbo_stream.replace('cart',
-                                                   partial: 'cart/cart',
-                                                   locals: { cart: @cart }),
-                              turbo_stream.replace(@product)]
-      end
-    end
+
+
   end
 
   def remove
